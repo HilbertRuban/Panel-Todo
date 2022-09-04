3c2814e5a3dfe667cbdbf1679568a5aa13af3a98
Hilbert <hilbertruban53@gmail.com>
2022-09-04 16:16:35 +0530

    added sign in form

--- src/components/SignInModal.jsx
+++ src/components/SignInModal.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SignInModal = () => {
   return (
     <>
-    <div className=" h-screen w-screen"></div>
+    
     <div id="authentication-modal" tabIndex="-1" className=" overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center flex" aria-modal="true" role="dialog">
     <div className="relative p-4 w-full max-w-md h-full md:h-auto">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
